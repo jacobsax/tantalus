22a6668411590e36c6753ec00c0e1c4deba8e686
Filipe Oliveira <contato@fmoliveira.com.br>
2015-08-01 23:17:35 -0300

    Fixing indentation.

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ app.on('window-all-closed', function() {
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform != 'darwin') {
-	app.quit();
+		app.quit();
 	}
 });
 
@@ -28,9 +28,9 @@ app.on('ready', function() {
 
 	// Emitted when the window is closed.
 	mainWindow.on('closed', function() {
-	// Dereference the window object, usually you would store windows
-	// in an array if your app supports multi windows, this is the time
-	// when you should delete the corresponding element.
-	mainWindow = null;
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
 	});
 });
