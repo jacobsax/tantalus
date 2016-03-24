be5e48a354afb2cb9c74f3515a39bb7559a09515
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-24 12:39:15 -0300

    Local storage for the notes service.

--- src/app/services/notes.js
+++ src/app/services/notes.js
@@ -2,10 +2,13 @@
 
 'use strict'
 
+// Load modules
+var uuid = require('node-uuid')
+
 /**
  * Service to manager user's notes.
  */
-function NotesService () {
+function NotesService (localStorageService) {
   var notes = []
 
   /**
@@ -17,14 +20,35 @@ function NotesService () {
   }
 
   /**
+   * Create initial notes for a new user.
+   */
+  function initNotes () {
+    notes = []
+    newNote('Welcome to Tantalus!', 'Tantalus is an alternative cross-platform client for Evernote.\n\nIt still stores everything in local storage, as we\'re in process of creating a great UI to integrate with the Evernote platform later.\n\nThanks for trying out Tantalus!')
+  }
+
+  /**
+   * Create a new note.
+   * @param  {String} noteTitle New note title.
+   * @param  {String} noteBody  New note body.
+   */
+  function newNote(noteTitle, noteBody) {
+    notes.push({
+      id: uuid.v4(),
+      title: noteTitle,
+      body: noteBody
+    })
+  }
+
+  /**
    * Load notes from the underlying storage.
    */
   function loadNotes () {
-    notes.push({
-      id: 1,
-      title: 'Lorem ipsum',
-      body: 'dolor sit amet'
-    })
+    notes = localStorageService.get('Notes')
+
+    if (!notes) {
+      initNotes()
+    }
   }
 
   // Load notes immediately
