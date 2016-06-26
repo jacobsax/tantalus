0d708c3de91ff3c5770c5b3cce2b756a648028f8
Filipe Oliveira <contato@fmoliveira.com.br>
2016-06-26 16:43:17 -0300

    Improved npm scripts structure.

---
+++ dist/main.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const electron = require('electron')
+const path = require('path')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+// Keeping this reference will prevent garbage collector from closing the app
+var mainWindow = null
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+  // OS X apps use to hide instead of closing
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// Load the main window when Electron has been initialised
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    'width': 800,
+    'height': 600,
+    'min-width': 500,
+    'min-height': 300,
+    'frame': false
+  })
+
+  mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
+
+  // Dereference it when window is closed
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+})
